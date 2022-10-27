6f3ed24a655ab3067e12e80f68621d9a3320d65d
hauntarl <sameer.mungole@gmail.com>
2022-10-27 16:04:04 -0700

    add init and cleanup methods

--- UrinalsTests.java
+++ UrinalsTests.java
@@ -1,8 +1,22 @@
 import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UrinalsTests {
+    private Urinals urinals = null;
+
+    @BeforeEach
+    void setup() {
+        urinals = new Urinals();
+    }
+
+    @AfterEach
+    void clean() {
+        urinals = null;
+    }
+
     @Test
     void testIsValidTrue() {
         System.out.println("====== Sameer Mungole == TEST ONE EXECUTED =======");
@@ -11,7 +25,6 @@ public class UrinalsTests {
                 "0000", "0001", "1000", "1001", "0100",
                 "0101010", "1010101", "1001001", "1001001001"
         };
-        Urinals urinals = new Urinals();
         for (String test : cases) {
             Boolean actual = urinals.isValid(test);
             assertTrue(String.format("%s should return true", test), actual);
