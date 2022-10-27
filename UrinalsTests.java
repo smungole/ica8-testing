baad5901917a5a3acfe59faaebb356ba7de5e382
hauntarl <sameer.mungole@gmail.com>
2022-10-27 15:16:43 -0700

    create a new test case for isValid

---
+++ UrinalsTests.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UrinalsTests {
+    @Test
+    void testIsValidTrue() {
+        System.out.println("====== Sameer Mungole == TEST ONE EXECUTED =======");
+
+        String[] cases = { "1001" };
+        Urinals urinals = new Urinals();
+        for (String test : cases) {
+            Boolean actual = urinals.isValid(test);
+            assertTrue(String.format("%s should return true", test), actual);
+        }
+    }
+}
