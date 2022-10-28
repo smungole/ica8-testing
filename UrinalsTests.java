a4e5aedaa54aa094e29cd526bae28c4b11230941
hauntarl <sameer.mungole@gmail.com>
2022-10-27 22:34:40 -0700

    remove redundant import

--- UrinalsTests.java
+++ UrinalsTests.java
@@ -3,7 +3,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
